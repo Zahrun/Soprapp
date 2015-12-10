99fe7ec3461cea320d735a2455e74e221dcd7380
Zorander <alexandre.angleraud@gmail.com>
2015-12-10 19:52:50 +0100

    test

--- Android/app/src/main/java/fr/insa/gei/soprapp/ActivityTest.java
+++ Android/app/src/main/java/fr/insa/gei/soprapp/ActivityTest.java
@@ -11,7 +11,7 @@ import fr.insa.gei.soprapp.dto.Sites;
 
 public class ActivityTest extends AppCompatActivity {
 
-    @Override
+ 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_test);
