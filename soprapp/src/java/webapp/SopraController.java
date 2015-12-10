9a93fdd805bcb0ee12eff32643eea6ea477ba0f8
gautierenaud <gautierenaud@gmail.com>
2015-12-10 19:34:10 +0100

    start implementing spring

---
+++ soprapp/src/java/webapp/SopraController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author gautierenaud
+ */
+//@WebServlet(name = "SopraController", urlPatterns = {"/SopraController"})
+@RestController
+public class SopraController{
+
+    @RequestMapping("/lala")
+    public String lala(){
+        return "lala";
+    }
+
+}
