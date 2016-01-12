49312e804a5e90e59813f6a0fd705b749e21472c
Zahrun <s141842@student.dtu.dk>
2016-01-12 12:38:11 +0100

    Ajout d'une TODOLIST

---
+++ Android/app/src/main/java/gei/soprapp/TODOLIST.java
@@ -0,0 +1,15 @@
+package gei.soprapp;
+
+/**
+ * Created by Clément Baudouin on 12/01/2016.
+ */
+public class TODOLIST {
+    // TODO envoyer requette trouver salle et afficher resultat
+    // TODO faire une reservation après le résultat
+
+    // TODO afficher reservations des salles quand on apuie dessus dans salle favorite
+    // TODO afficher detail reservation quand on apuie dessus dans reservations + possibilité de supprimer
+
+    // TODO choix des salles favorites uniquement parmis celles du site
+    // TODO notifications
+}
